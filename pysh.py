29ce9cc1d1371eb496aa62110ed30d2143d70117
Chris Morgan <chris.j.r.morgan@gmail.com>
2014-07-30 09:54:40 +1200

    added some basic functionality

--- pysh.py
+++ pysh.py
@@ -1,9 +1,24 @@
 #! /usr/bin/env python3
 
 import os
+from subprocess import call
 
 def main():
-    pass
+
+    while True:
+
+        # Grab user input and split it up into command and args.
+        command_array = input('=> ').split()
+        command, args = command_array[0], command_array[1:]
+
+        # Switch statement to determine command behaviour.
+        if command == 'exit':
+            break
+        elif command == 'cd':
+            os.chdir(' '.join(args))
+        else:
+            print(call([command] + args))
+
 
 if __name__ == '__main__':
     main()
